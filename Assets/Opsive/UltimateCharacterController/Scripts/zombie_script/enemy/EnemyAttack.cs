2addfa8244a70b47b27c29e9154b662e5ea57781
Backendeng <backendeng0417@gmail.com>
2022-10-11 15:39:25 -0400

    Update version

---
+++ Assets/Opsive/UltimateCharacterController/Scripts/zombie_script/enemy/EnemyAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Opsive.UltimateCharacterController.Traits;
+using Opsive.UltimateCharacterController.Character;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+	public int Damage = 80;
+	public EnemyHealth Enemyhealth;
+
+	void OnTriggerEnter (Collider PlayerCollider)
+	{
+
+
+		if (PlayerCollider.tag.Equals ("Player") && Enemyhealth.health > 0) {
+			var health = PlayerCollider.GetComponentInParent<UltimateCharacterLocomotion>();
+			if(health != null) {
+				health.GetComponent<CharacterHealth>().Damage(10);
+			}
+
+			// CapsuleColliderPositioner player_capsule = PlayerCollider.gameObject.GetComponent<CapsuleColliderPositioner> ();
+			// Transform player = player_capsule.FirstEndCapTarget;
+			// Health	playerHealth = PlayerCollider.gameObject.GetComponent<Health> ();
+			
+			// playerHealth.SetDamage (Damage);
+
+		}
+
+	}
+
+
+
+}
---
+++ Assets/Opsive/UltimateCharacterController/Scripts/zombie_script/enemy/EnemyFireAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireAttack : MonoBehaviour
+{
+
+	public int Damage = 10;
+
+	void OnParticleCollision (GameObject other)
+	{
+
+	
+		if (other.tag.Equals ("Player")) {
+
+			EHealth playerHealth = other.GetComponent<EHealth> ();
+
+			playerHealth.SetDamage (Damage);
+
+		}
+
+	}
+
+
+
+}
